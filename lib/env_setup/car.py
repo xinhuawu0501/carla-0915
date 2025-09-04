9455ac75a667ddd2efb85f70ba5d8c00f0a303de
xinhuawu0501 <b06106035@ntu.edu.tw>
2025-09-04 15:04:55 +0800

    refactor: move all car-related attr and mathods to `Car` class

--- lib/env_setup/car.py
+++ lib/env_setup/car.py
@@ -1,13 +1,20 @@
 import carla
 import random
 import queue
+from enum import Enum
 
 from constants.camera import IMG_X, IMG_Y
+from util.image_processing import cv_display, process_rgb_img, process_semantic_img
+
+class Sensor(Enum):
+    RGB = 'RGB'
+    SEMANTIC = 'SEMANTIC'
+    COLSEN = 'COLSEN'
 
 DEFAULT_SENSOR_OPTION = {
-    'rgb': False,
-    'semantic': True,
-    'colsen': True
+    Sensor.RGB: False,
+    Sensor.SEMANTIC: True,
+    Sensor.COLSEN: True
 }
 
 DEFAULT_CAM_ATTR = {
@@ -16,22 +23,21 @@ DEFAULT_CAM_ATTR = {
     'fov': 110
 }
 
-class CarBaseAgent:
+class Car:
     collision_data = []
+    sensors = []
 
     def __init__(
             self, 
             world, 
             sensor_options=DEFAULT_SENSOR_OPTION, 
             sp=None, 
-            route=None, 
             cam_attr=DEFAULT_CAM_ATTR):
         self.world = world
         self.sensor_options = sensor_options
         self.vehicle_bps = self.world_bp.filter('vehicle.*.*')
         self.all_sps = self.world.get_spawn_points()
 
-        self.route = route
         self.cam_attr = cam_attr
         self.sp = sp
         self.car = None
@@ -39,6 +45,8 @@ class CarBaseAgent:
         self.image_queue = queue.Queue
         self.semantic_img_queue = queue.Queue
         
+        self.spawn_self()
+
     def spawn_self(self):
         try:
             self.vehicle_bp = self.vehicle_bps[0]
@@ -51,7 +59,7 @@ class CarBaseAgent:
 
             cam_transform = carla.Transform(carla.Location(x=-4, z=3), carla.Rotation(pitch=-15))
 
-            if self.sensor_options.get('rgb'):
+            if self.sensor_options.get(Sensor.RGB):
                 self.cam_bp = self.world_bp.find('sensor.camera.rgb')
                 self.cam_bp.set_attribute('image_size_x', str(self.cam_attr.x))
                 self.cam_bp.set_attribute('image_size_y', str(self.cam_attr.y))
@@ -59,8 +67,9 @@ class CarBaseAgent:
 
                 self.rgb_cam = self.world.spawn_actor(self.cam_bp, cam_transform, attach_to=self.car)
                 self.rgb_cam.listen(lambda img: self.image_queue.put(img))
+                self.sensors.append(self.rgb_cam)
 
-            if self.sensor_options.get('semantic'):
+            if self.sensor_options.get(Sensor.SEMANTIC):
                 self.semantic_img_queue = queue.Queue()
                 cam_bp = self.world_bp.find('sensor.camera.semantic_segmentation')
                 cam_bp.set_attribute('image_size_x', str(self.cam_attr.x))
@@ -68,12 +77,69 @@ class CarBaseAgent:
                 cam_bp.set_attribute('fov', str(self.cam_attr.fov))
                 self.semantic_cam = self.world.spawn_actor(cam_bp, cam_transform, attach_to=self.car)
                 self.semantic_cam.listen(lambda img: self.semantic_img_queue.put(img))
+                self.sensors.append(self.semantic_cam)
 
-            if self.sensor_options.get('colsen'):
+            if self.sensor_options.get(Sensor.COLSEN):
                 self.colsen_bp = self.world_bp.find('sensor.other.collision')
                 self.colsen = self.world.spawn_actor(self.colsen_bp, carla.Transform(), attach_to=self.car)
                 self.colsen.listen(lambda event: self.collision_data.append(event))
+                self.sensors.append(self.colsen)
+
         except Exception as e:
             print(f'spawn car failed {e}')
 
 
+    def get_raw_img_from_q(self, img_type = Sensor.SEMANTIC):
+        try:
+            if img_type == Sensor.SEMANTIC:
+                raw_img = self.semantic_img_queue.get(block=False)
+            elif img_type == Sensor.RGB:
+                raw_img = self.image_queue.get(block=False)
+            return raw_img
+        except queue.Empty:
+            print(f'{img_type} queue empty')
+
+    def display_img(self, raw, img_type = Sensor.SEMANTIC):
+        try:
+            if img_type == Sensor.SEMANTIC:
+                processed = process_semantic_img(raw)
+            elif img_type == Sensor.RGB:
+                processed = process_rgb_img(raw)
+
+            cv_display(processed)
+        except Exception as e:
+            print(e)
+
+    def clear_all_q(self):
+        try:
+            while not self.image_queue.empty():
+                try:
+                    self.image_queue.get_nowait()
+                except queue.Empty:
+                    break
+
+            while not self.semantic_img_queue.empty():
+                try:
+                    self.semantic_img_queue.get_nowait()
+                except queue.Empty:
+                    break
+        except Exception as e:
+            print(e)
+
+    def is_at_traffic_light(self):
+        is_at = self.car.is_at_traffic_light()
+        if is_at:
+            print(f'is at traffic light: {is_at}')
+            print(f'light: {self.car.get_traffic_light_state()}')
+        
+        return is_at
+    
+    def apply_control(self, control: carla.VehicleControl):
+        self.car.apply_control(control)
+    
+    def close(self):
+        for sensor in self.sensors:
+            sensor.stop()
+            sensor.destroy()
+        
+        self.car.destroy()
